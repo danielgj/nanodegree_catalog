92ae1c2dca81e74fc4c98117c073dd7c24aa4ee9
Daniel Garcia Jones <danielgj@gmail.com>
2019-03-11 20:13:18 +0100

    Added routes

--- catalog.py
+++ catalog.py
@@ -14,10 +14,42 @@ import requests
 
 app = Flask(__name__)
 
-# Show Home
+
+@app.route('/login')
+def showLogin():
+    return "Login"
+
+@app.route('/logout')
+def showLogout():
+    return "Logout"
+
 @app.route('/')
 def showHome():
-    return "Up and runing"
+    return "Home"
+
+@app.route('/catalog/<string:category>/items/')
+def showItems(category):
+    return "Items"
+
+@app.route('/catalog/<string:category>/items/<string:item>')
+def showItemDetail(category, item):
+    return "Item"
+
+@app.route('/catalog/<string:category>/items/new')
+def newItem(category):
+    return "New Item"
+
+@app.route('/catalog/<string:item>/edit')
+def editItem(item):
+    return "Item edit "
+
+@app.route('/catalog/<string:item>/delete')
+def deleteItem(item):
+    return "Item delete"
+
+@app.route('/catalog.json')
+def showJSON():
+    return "JSON output"
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
