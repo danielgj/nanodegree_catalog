fa3744efccdb191a50cc7b4fba955e3f207a5e04
Daniel Garcia Jones <danielgj@gmail.com>
2019-03-11 19:37:12 +0100

    Added Category and Item ORMs

--- database_setup.py
+++ database_setup.py
@@ -12,3 +12,21 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    name =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    title =Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    category_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+    
